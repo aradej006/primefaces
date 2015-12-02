d13f2ba5611fa90160e1e4bfdac0785a330fe6f3
CzarR1 <czarek.rozman@wp.pl>
2015-12-02 15:37:49 +0100

    sadasd

---
+++ src/main/java/eu/sii/pl/primefacestest/TestBean.java
@@ -0,0 +1,28 @@
+package eu.sii.pl.primefacestest;
+
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@RequestScoped
+@Named
+public class TestBean implements Serializable{
+
+    private String world;
+
+
+    @PostConstruct
+    public void sdas(){
+        world = "World";
+    }
+
+    public String getWorld() {
+        return world;
+    }
+
+    public void setWorld(String world) {
+        this.world = world;
+    }
+}
